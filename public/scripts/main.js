430e2ff61de666cb11db5cf1f3d5506d8077b025
Sam Hager <sam.hager11@gmail.com>
2015-11-25 16:14:44 -0800

    created reverse geolocating to define city and state for user position instead of ip get

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-console.log("===========weatherU api initiated!!!!!!!");
+
 
 //Formatting state name to abbreviation
 function convert_state(name, to) {
@@ -40,17 +40,77 @@ function convert_state(name, to) {
 
 //Setup variables to be used for API searches
 var yelpURL = 'http://api.yelp.com/v2/search'
-var conditionsURL = 'https://api.wunderground.com/api/726c0ba149d8a811/conditions/q/';
+var conditionsURL = 'http://api.wunderground.com/api/726c0ba149d8a811/conditions/q/';
 var $inpCtry = $('#appendCountryUrl')
 var $inpState = $('#appendStateUrl')
 var $inpCity = $('#appendCityUrl')
 var state = '';
 var city = '';
+
 // create placeholder variables for User Position - used for Uber
 var userLatitude;
 var userLongitude;
 
-//Get user location
+//for Weather call
+var startLatitude;
+var startLongitude;
+var stateAbbrev;
+var stateFull;
+var city;
+
+
+navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+//Get the latitude and the longitude;
+function successFunction(position) {
+  var startLatitude = position.coords.latitude;
+  var startLongitude = position.coords.longitude;
+  console.log(startLongitude)
+  codeLatLng(startLatitude, startLongitude)
+}
+
+function errorFunction(){
+  alert("Geocoder failed");
+}
+
+//Get Google GeoCode API based on user startLatitude and startLongitude
+//Parse through result object to return city and state for use in weather call
+function codeLatLng(lat, lng) {
+
+  $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" +lat + "," + lng + "&key=AIzaSyDtL6P7cJfHJg3LQARwMlMXNl7iCGWm_8I", function(data,status){
+
+    // for (var i = 0; i < data.results.length; i++) {
+    //   console.log(data.results[i])
+    // }
+    if (data.results[1]) {
+
+    for (var i=0; i<data.results[0].address_components.length; i++) {
+      for (var b=0;b<data.results[0].address_components[i].types.length;b++) {
+
+    //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+        if (data.results[0].address_components[i].types[b] == "administrative_area_level_1") {
+            //this is the state object
+            state= data.results[0].address_components[i];
+          }
+        if (data.results[0].address_components[i].types[b] == "locality") {
+            //this is the city object
+            city = data.results[0].address_components[i];
+          }
+        }
+      }
+      //state data
+      city = city.short_name
+      stateAbbrev = state.short_name
+      stateFull = state.long_name
+      console.log(city + " " + stateAbbrev + " " + stateFull)
+
+      } else {
+        alert("No results found");
+      }
+  })
+}
+
+
+//Get user location continuously for yelp and uber responses
 navigator.geolocation.watchPosition(function(position) {
     console.log(position);
     // Update latitude and longitude
@@ -58,33 +118,18 @@ navigator.geolocation.watchPosition(function(position) {
     userLongitude = position.coords.longitude;
 });
 
-//Uber Setup ==================================================================
-//Uber Secret: ooJn0ntCQYlovNZQBZ0j7VPvI43OXqd0PpN569Km
-
-// CLIENT ID
-// O4DQyt8u2XTuKGUAft4YZlzS9Yni4QQH
-
-// SERVER TOKEN
-// 7zMUXj4LgZviCq0xfEdpgLn6LsBlENzR3EYlUItz
-
-
-//Uber API endpoints
-//    /v1/products
-//    /v1/estimates/price
-//    /v1/estimates/time
-//    /v1/promotions
 
 
 
 //Get IP info for current user to be able to pass city and state to weather search
-$.get("https://ipinfo.io", function(login) {
-    console.log(login.city, login.region);
-     state = convert_state(login.region, 'abbrev');
-     city  = login.city;
+$.get("http://ipinfo.io", function(ip_info) {
+    console.log(ip_info.city, ip_info.region);
+     state = convert_state(ip_info.region, 'abbrev');
+     city  = ip_info.city;
 
-    console.log($inpCity);
     $inpCity.val(city)
     $inpState.val(state)
+    console.log($inpState.val())
 }, "jsonp");
 
 //Get Weather and Call APIs Uber and Yelp for CURRENT location (IP and GEO Coordinates)
@@ -96,10 +141,10 @@ window.onload = function(){
       console.log(data.current_observation.weather, data.current_observation.temperature_string)
       // AJAX call for Yelp API app using city and state from IP Info
       $.ajax({
-        url: '/yelp/' + $inpCity.val() + $inpState.val(),
+        url: '/yelp/' + city  + stateAbbrev,
         method: 'GET',
         success: function(data){
-          console.log(data)
+          // console.log(data)
           if(data.businesses){
             for (var i = 0; i < 5; i++){
               var business = data.businesses[i]
