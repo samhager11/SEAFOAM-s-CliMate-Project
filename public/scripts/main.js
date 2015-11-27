9a4785a2a07352df774d50de0f32c40866377932
Sam Hager <sam.hager11@gmail.com>
2015-11-27 11:55:38 -0800

    added weather checking functionality to main.js. Also removed hidden characters from main.js as they were causing an unexpected token error in chrome

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,41 +1,132 @@
 //Formatting state name to abbreviation
-function convert_state(name, to) {
-    var name = name.toUpperCase();
-    var states = new Array(                         {'name':'Alabama', 'abbrev':'AL'},          {'name':'Alaska', 'abbrev':'AK'},
-        {'name':'Arizona', 'abbrev':'AZ'},          {'name':'Arkansas', 'abbrev':'AR'},         {'name':'California', 'abbrev':'CA'},
-        {'name':'Colorado', 'abbrev':'CO'},         {'name':'Connecticut', 'abbrev':'CT'},      {'name':'Delaware', 'abbrev':'DE'},
-        {'name':'Florida', 'abbrev':'FL'},          {'name':'Georgia', 'abbrev':'GA'},          {'name':'Hawaii', 'abbrev':'HI'},
-        {'name':'Idaho', 'abbrev':'ID'},            {'name':'Illinois', 'abbrev':'IL'},         {'name':'Indiana', 'abbrev':'IN'},
-        {'name':'Iowa', 'abbrev':'IA'},             {'name':'Kansas', 'abbrev':'KS'},           {'name':'Kentucky', 'abbrev':'KY'},
-        {'name':'Louisiana', 'abbrev':'LA'},        {'name':'Maine', 'abbrev':'ME'},            {'name':'Maryland', 'abbrev':'MD'},
-        {'name':'Massachusetts', 'abbrev':'MA'},    {'name':'Michigan', 'abbrev':'MI'},         {'name':'Minnesota', 'abbrev':'MN'},
-        {'name':'Mississippi', 'abbrev':'MS'},      {'name':'Missouri', 'abbrev':'MO'},         {'name':'Montana', 'abbrev':'MT'},
-        {'name':'Nebraska', 'abbrev':'NE'},         {'name':'Nevada', 'abbrev':'NV'},           {'name':'New Hampshire', 'abbrev':'NH'},
-        {'name':'New Jersey', 'abbrev':'NJ'},       {'name':'New Mexico', 'abbrev':'NM'},       {'name':'New York', 'abbrev':'NY'},
-        {'name':'North Carolina', 'abbrev':'NC'},   {'name':'North Dakota', 'abbrev':'ND'},     {'name':'Ohio', 'abbrev':'OH'},
-        {'name':'Oklahoma', 'abbrev':'OK'},         {'name':'Oregon', 'abbrev':'OR'},           {'name':'Pennsylvania', 'abbrev':'PA'},
-        {'name':'Rhode Island', 'abbrev':'RI'},     {'name':'South Carolina', 'abbrev':'SC'},   {'name':'South Dakota', 'abbrev':'SD'},
-        {'name':'Tennessee', 'abbrev':'TN'},        {'name':'Texas', 'abbrev':'TX'},            {'name':'Utah', 'abbrev':'UT'},
-        {'name':'Vermont', 'abbrev':'VT'},          {'name':'Virginia', 'abbrev':'VA'},         {'name':'Washington', 'abbrev':'WA'},
-        {'name':'West Virginia', 'abbrev':'WV'},    {'name':'Wisconsin', 'abbrev':'WI'},        {'name':'Wyoming', 'abbrev':'WY'}
-        );
-    var returnthis = false;
-    $.each(states, function(index, value){
-        if (to == 'name') {
-            if (value.abbrev == name){
-                returnthis = value.name;
-                return false;
-            }
-        } else if (to == 'abbrev') {
-            if (value.name.toUpperCase() == name){
-                returnthis = value.abbrev;
-                return false;
-            }
-        }
-    });
-    return returnthis;
+//NOT REQUIRED NOW THAT WE ARE USING GEOLOCATION INSTEAD OF IP
+// function convert_state(name, to) {
+//     var name = name.toUpperCase();
+//     var states = new Array(                         {'name':'Alabama', 'abbrev':'AL'},          {'name':'Alaska', 'abbrev':'AK'},
+//         {'name':'Arizona', 'abbrev':'AZ'},          {'name':'Arkansas', 'abbrev':'AR'},         {'name':'California', 'abbrev':'CA'},
+//         {'name':'Colorado', 'abbrev':'CO'},         {'name':'Connecticut', 'abbrev':'CT'},      {'name':'Delaware', 'abbrev':'DE'},
+//         {'name':'Florida', 'abbrev':'FL'},          {'name':'Georgia', 'abbrev':'GA'},          {'name':'Hawaii', 'abbrev':'HI'},
+//         {'name':'Idaho', 'abbrev':'ID'},            {'name':'Illinois', 'abbrev':'IL'},         {'name':'Indiana', 'abbrev':'IN'},
+//         {'name':'Iowa', 'abbrev':'IA'},             {'name':'Kansas', 'abbrev':'KS'},           {'name':'Kentucky', 'abbrev':'KY'},
+//         {'name':'Louisiana', 'abbrev':'LA'},        {'name':'Maine', 'abbrev':'ME'},            {'name':'Maryland', 'abbrev':'MD'},
+//         {'name':'Massachusetts', 'abbrev':'MA'},    {'name':'Michigan', 'abbrev':'MI'},         {'name':'Minnesota', 'abbrev':'MN'},
+//         {'name':'Mississippi', 'abbrev':'MS'},      {'name':'Missouri', 'abbrev':'MO'},         {'name':'Montana', 'abbrev':'MT'},
+//         {'name':'Nebraska', 'abbrev':'NE'},         {'name':'Nevada', 'abbrev':'NV'},           {'name':'New Hampshire', 'abbrev':'NH'},
+//         {'name':'New Jersey', 'abbrev':'NJ'},       {'name':'New Mexico', 'abbrev':'NM'},       {'name':'New York', 'abbrev':'NY'},
+//         {'name':'North Carolina', 'abbrev':'NC'},   {'name':'North Dakota', 'abbrev':'ND'},     {'name':'Ohio', 'abbrev':'OH'},
+//         {'name':'Oklahoma', 'abbrev':'OK'},         {'name':'Oregon', 'abbrev':'OR'},           {'name':'Pennsylvania', 'abbrev':'PA'},
+//         {'name':'Rhode Island', 'abbrev':'RI'},     {'name':'South Carolina', 'abbrev':'SC'},   {'name':'South Dakota', 'abbrev':'SD'},
+//         {'name':'Tennessee', 'abbrev':'TN'},        {'name':'Texas', 'abbrev':'TX'},            {'name':'Utah', 'abbrev':'UT'},
+//         {'name':'Vermont', 'abbrev':'VT'},          {'name':'Virginia', 'abbrev':'VA'},         {'name':'Washington', 'abbrev':'WA'},
+//         {'name':'West Virginia', 'abbrev':'WV'},    {'name':'Wisconsin', 'abbrev':'WI'},        {'name':'Wyoming', 'abbrev':'WY'}
+//         );
+//     var returnthis = false;
+//     $.each(states, function(index, value){
+//         if (to == 'name') {
+//             if (value.abbrev == name){
+//                 returnthis = value.name;
+//                 return false;
+//             }
+//         } else if (to == 'abbrev') {
+//             if (value.name.toUpperCase() == name){
+//                 returnthis = value.abbrev;
+//                 return false;
+//             }
+//         }
+//     });
+//     return returnthis;
+// }}
+
+//Weather Pattern Determination
+var weatherPattern;
+var weatherTypes = ["sunny","rainy","snowy","cloudy"];
+var wundergroundType;
+
+var weatherObject = { sunny:["Clear",
+			"Unknown",
+			"Scattered Clouds"],
+	rainy:
+		["Light Drizzle",	"Heavy Drizzle",
+		"Light Rain",	"Heavy Rain",
+		"Light Mist",	"Heavy Mist",
+		"Light Spray",	"Heavy Spray",
+		"Light Rain Mist",	"Heavy Rain Mist",
+		"Light Rain Showers",	"Heavy Rain Showers",
+		"Light Thunderstorm",	"Heavy Thunderstorm",
+		"Light Thunderstorms and Rain",	"Heavy Thunderstorms and Rain",
+		"Light Thunderstorms and Ice Pellets",	"Heavy Thunderstorms and Ice Pellets",
+		"Light Thunderstorms with Hail",	"Heavy Thunderstorms with Hail",
+		"Light Thunderstorms with Small Hail",	"Heavy Thunderstorms with Small Hail",
+		"Light Freezing Drizzle",	"Heavy Freezing Drizzle",
+		"Light Freezing Rain",	"Heavy Freezing Rain",
+		"Light Freezing Fog",	"Heavy Freezing Fog",
+		"Squalls",
+		"Unknown Precipitation"],
+	snowy:
+		["Light Snow Grains",	"Heavy Snow Grains",
+			"Light Ice Crystals",	"Heavy Ice Crystals",
+			"Light Ice Pellets",	"Heavy Ice Pellets",
+			"Light Low Drifting Snow",	"Heavy Low Drifting Snow",
+			"Light Snow",	"Heavy Snow",
+			"Light Blowing Snow",	"Heavy Blowing Snow",
+			"Light Snow Showers",	"Heavy Snow Showers",
+			"Light Snow Blowing Snow Mist",	"Heavy Snow Blowing Snow Mist",
+			"Light Ice Pellet Showers",	"Heavy Ice Pellet Showers",
+			"Light Hail Showers",	"Heavy Hail Showers",
+			"Light Small Hail Showers",	"Heavy Small Hail Showers",
+			"Light Thunderstorms and Snow",	"Heavy Thunderstorms and Snow",
+			"Small Hail"],
+	cloudy:
+		["Light Fog",	"Heavy Fog",
+			"Light Fog Patches",	"Heavy Fog Patches",
+			"Light Smoke",	"Heavy Smoke",
+			"Light Volcanic Ash",	"Heavy Volcanic Ash",
+			"Light Widespread Dust",	"Heavy Widespread Dust",
+			"Light Sand",	"Heavy Sand",
+			"Light Haze",	"Heavy Haze",
+			"Light Dust Whirls",	"Heavy Dust Whirls",
+			"Light Sandstorm",	"Heavy Sandstorm",
+			"Light Low Drifting Sand",	"Heavy Low Drifting Sand",
+			"Light Blowing Widespread Dust",	"Heavy Blowing Widespread Dust",
+			"Light Blowing Sand",	"Heavy Blowing Sand",
+			"Patches of Fog",
+			"Shallow Fog",
+			"Partial Fog",
+			"Overcast",
+			"Partly Cloudy",
+			"Mostly Cloudy",
+			"Funnel Cloud"]
+	}
+
+
+function determineWeather(forecast,checkForecast){
+		if(!wundergroundType){
+		weatherPattern = rainyDay
+	} else {
+		for (var i = 0; i < weatherObject.sunny.length; i++) {
+			if(weatherObject.sunny[i]===wundergroundType){
+				weatherPattern = "sunnyDay"
+			}
+		}
+		for (var i = 0; i < weatherObject.cloudy.length; i++) {
+			if(weatherObject.cloudy[i]===wundergroundType){
+				weatherPattern = "cloudyDay"
+			}
+		}
+		for (var i = 0; i < weatherObject.snowy.length; i++) {
+			if(weatherObject.snowy[i]===wundergroundType){
+				weatherPattern = "snowyDay"
+			}
+		}
+		for (var i = 0; i < weatherObject.rainy.length; i++) {
+			if(weatherObject.rainy[i]===wundergroundType){
+				weatherPattern = "rainyDay"
+			}
+		}
+	} console.log(wundergroundType + " = " + weatherPattern)
 }
-​
+
+
 //Setup variables to be used for API searches
 var yelpURL = 'http://api.yelp.com/v2/search'
 var conditionsURL = 'https://api.wunderground.com/api/726c0ba149d8a811/conditions/q/'
@@ -45,8 +136,7 @@ var $inpState = $('#appendStateUrl')
 var $inpCity = $('#appendCityUrl')
 var city;
 var state;
-​
-​
+
 // create placeholder variables for User Position - used for Uber
 var userLatitude;
 var userLongitude;
@@ -55,19 +145,18 @@ var yelpLongitude;
 var uberPrice;
 var uberTimeMin;
 var uberDistMiles;
-​
+
 // Uber API Constants
 var uberClientId = "O4DQyt8u2XTuKGUAft4YZlzS9Yni4QQH";
 var uberServerToken = "7zMUXj4LgZviCq0xfEdpgLn6LsBlENzR3EYlUItz";
-​
-​
+
+
 //Twitter socket io instantiation
 var socket = io();
 socket.on('connect', function(){
   console.log('Connected!')
 })
-​
-​
+
 //Get user location
 //for Weather call
 var startLatitude;
@@ -75,9 +164,9 @@ var startLongitude;
 var geoStateAbrev;
 var geoStateFull;
 var geoCity;
-​
+
 ///////////////////////////////////////////////////////////////////////////////
-​
+
 //Get IP info for current user to be able to pass city and state to weather search
 //NOT USED DUE TO UNSECURED URL GET REQUEST - HEROKU DID NOT ACCEPT
 // $.get("http://ipinfo.io", function(ip_info) {
@@ -89,9 +178,8 @@ var geoCity;
 //     $inpState.val(state)
 //
 // }, "jsonp");
-​
+
 //Get user location continuously for yelp and uber responses
-​
 navigator.geolocation.watchPosition(function(position) {
     console.log(position);
     // Update latitude and longitude
@@ -99,7 +187,6 @@ navigator.geolocation.watchPosition(function(position) {
     userLongitude = position.coords.longitude;
 });
 
-​
 navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
 //Get the latitude and the longitude;
 function successFunction(position) {
@@ -108,11 +195,11 @@ function successFunction(position) {
   // console.log(startLongitude)
   getLocationAndMakeCalls(startLatitude, startLongitude)
 }
-​
+
 function errorFunction(){
   alert("Geocoder failed");
 }
-​
+
 //Get Google GeoCode API based on user startLatitude and startLongitude
 //Parse through result object to return city and state for use in weather call
 //Make calls to Weather, Yelp, Uber, and Twitter based on user location
@@ -152,6 +239,9 @@ function getLocationAndMakeCalls(lat, lng) {
       method: 'GET',
       success: function (data) {
         console.log(data.current_observation.weather, data.current_observation.temperature_string)
+        wundergroundType = data.current_observation.weather
+        //Use this function to pass weather type returned from weather underground
+        determineWeather(wundergroundType,weatherObject)
         $('#temperature').text(data.current_observation.temperature_string)
       }
     })
@@ -198,9 +288,7 @@ function getLocationAndMakeCalls(lat, lng) {
     socket.emit('updateTerm', search_term)
   })
 }
-​
-​
-​
+
 //Ajax Get for Uber price estimate based on User location
 function getEstimatesForUserLocation(latitude,longitude) {
   $.ajax({
@@ -229,8 +317,7 @@ function getEstimatesForUserLocation(latitude,longitude) {
     }
   });
 }
-​
-​
+
 //Get Weather and Call APIs Uber and Yelp for SEARCH location (GEO Coordinates)
 //Stream twitter based on input city
 $('#submit').on('click', function(){
@@ -244,6 +331,8 @@ $('#submit').on('click', function(){
     method: 'GET',
     success: function (data) {
       console.log(data.current_observation.weather, data.current_observation.temperature_string)
+      wundergroundType = data.current_observation.weather;
+      determineWeather(wundergroundType,weatherObject)
       }
     })
       // AJAX call for Yelp API app using city and state from user location info
--- public/scripts/rainyday.js
+++ public/scripts/rainyday.js
@@ -1,6 +1,5 @@
 
 
-
  // * Defines a new instance of the rainyday.js.
  // * param options options element with script parameters
  // * param canvas to be used (if not defined a new one will be created)
